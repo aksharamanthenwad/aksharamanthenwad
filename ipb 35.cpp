2e38ad6f6e35b6f23da63d591046588bb39d156b
aksharamanthenwad <aksharamanthenwad@gmail.com>
2025-03-17 17:26:47 +0530

    Add files via upload
    ipb 35

---
+++ ipb 35.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i=2,count=i;
+	printf("enter any number");
+	while(i<n)
+	{
+		if(n%i=0)
+		{
+			count=count+1;
+		}
+		i++;
+	}
+	if(count==0)
+	{
+	printf("prime number");	
+	}
+	else
+	{
+		printf("not a prime number");
+	}
+}
