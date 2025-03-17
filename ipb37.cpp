c839e6f26a37ac35908707c31ff9bfaee86bdcc7
aksharamanthenwad <aksharamanthenwad@gmail.com>
2025-03-17 17:27:57 +0530

    Add files via upload
    ipb 37

---
+++ ipb37.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter n value");
+	scanf("%d",&n);
+	{
+		printf("%d is even number",n);
+	}
+	else
+	{
+		printf("%d is odd number",n);
+	}
+	return 0;
+}
