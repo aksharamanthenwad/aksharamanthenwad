18b604e5b8a9f131288c6965c8f1eac3c4480d78
aksharamanthenwad <aksharamanthenwad@gmail.com>
2025-03-17 17:27:27 +0530

    Add files via upload
    ipb 36

---
+++ ipb36.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int f1,f2,f3,n,i;
+	printf("enter n");
+	scanf("%d",&n);
+	f1=0;
+	f2=1;
+	for (i=0;i,n;i++)
+	{
+		printf("%d",f1);
+		f3=f1=f2;
+		f1=f2;
+		f2=f3;
+	}
+	
+}
